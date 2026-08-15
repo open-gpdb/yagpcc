a0b1a2c25c460cbe6199b089f32c14a9297d731b
sanikadze <sanikadze@ya.ru>
2026-08-15 20:48:36 +0300

    style: reformat replicated guards in down migration

--- internal/sink/clickhouse/migrations/0001_init.down.sql
+++ internal/sink/clickhouse/migrations/0001_init.down.sql
@@ -1,7 +1,9 @@
-{{if .Replicated}}DROP TABLE IF EXISTS yagpcc.sessions ON CLUSTER '{cluster}';
+{{if .Replicated}}
+DROP TABLE IF EXISTS yagpcc.sessions ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS yagpcc.statements ON CLUSTER '{cluster}';
 DROP TABLE IF EXISTS yagpcc.segments ON CLUSTER '{cluster}';
-{{end}}DROP TABLE IF EXISTS yagpcc.segments_part{{if .Replicated}} ON CLUSTER '{cluster}'{{end}};
+{{end}}
+DROP TABLE IF EXISTS yagpcc.segments_part{{if .Replicated}} ON CLUSTER '{cluster}'{{end}};
 DROP TABLE IF EXISTS yagpcc.statements_part{{if .Replicated}} ON CLUSTER '{cluster}'{{end}};
 DROP TABLE IF EXISTS yagpcc.sessions_part{{if .Replicated}} ON CLUSTER '{cluster}'{{end}};
 DROP TABLE IF EXISTS yagpcc._yagpcc_meta;
